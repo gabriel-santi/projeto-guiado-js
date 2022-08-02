16fddeff4f73e0c7ae8837b21da8a8ad1a6c65ea
GabrielSantiago <marcosgabrielss23@gmail.com>
2022-08-02 12:46:23 -0300

    Arquivos js

---
+++ assets/js/form.js
@@ -0,0 +1,97 @@
+const urlOriginal = new URL(window.location.href);
+const id = urlOriginal.searchParams.get('person');
+
+if (id !== null) {
+    let peopleRaw = localStorage.getItem('people');
+    if (peopleRaw != null) {
+        people = JSON.parse(localStorage.getItem('people'))
+    } else {
+        people = [];
+    }
+
+    document.getElementById('name').value = people[id].name;
+    document.getElementById('phone').value = people[id].tel;
+    if (people[id].xp) {
+        document.getElementById('xp-yes').checked = true;
+    } else {
+        document.getElementById('xp-no').checked = true;
+    }
+}
+
+function testaFormulario(e) {
+    e.preventDefault();
+    //validar telefone 
+    /* for (i in e.target.elements['phone'].value) {
+        if ('0123456789'.indexOf(e.target.elements['phone'].value[i]) == -1) {
+            alert('Apenas números são permitidos!');
+
+            return false;
+        }
+
+        if (e.target.elements['phone'].value.length < 11) {
+        alert('O telefone deve conter 11 números!');
+
+        return false;
+    }
+
+    } */
+
+    const phonePattern = /[^0-9]+/g
+    if (phonePattern.test(e.target.elements['phone'].value.replace(/[-() ]/g, ''))) {
+        alert('Apenas números são permitidos!');
+
+        return false;
+    }
+
+    if (e.target.elements['phone'].value.replace(/[-() ]/g, '').length < 11) {
+        alert('O telefone deve conter 11 números!');
+
+        return false;
+    }
+
+    let people;
+    let peopleRaw = localStorage.getItem('people');
+    if (peopleRaw != null) {
+        people = JSON.parse(peopleRaw)
+    } else {
+        people = [];
+    }
+
+    if (id !== null) {
+        people[id] = {
+            name: e.target.elements['name'].value,
+            tel: e.target.elements['phone'].value,
+            xp: (e.target.elements['xp'].value == 'true')
+        }
+    } else {
+        people.push({
+            name: e.target.elements['name'].value,
+            tel: e.target.elements['phone'].value,
+            xp: (e.target.elements['xp'].value == 'true')
+        })
+    }
+
+    localStorage.setItem('people', JSON.stringify(people));
+
+    document.getElementById('goHome').click();
+}
+
+function testaCampoTelefone(e) {
+    e.preventDefault();
+
+    if (e.target.value.length == 0) {
+        e.target.value += '(';
+    }
+
+    if (e.target.value.length == 3) {
+        e.target.value += ') ';
+    }
+
+    if (e.target.value.length == 10) {
+        e.target.value += '-';
+    }
+
+    if ((/[0-9]/g).test(e.key) && e.target.value.length < 15) {
+        e.target.value += e.key;
+    }
+}
